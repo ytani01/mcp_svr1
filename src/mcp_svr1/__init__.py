55aa06a512d7533402186685473bf3c0d0dcbd48
Yoichi Tanibayashi <yoichi@tanibayashi.jp>
2025-09-16 23:54:47 +0900

    ADD: click_utils.py

--- src/mcp_svr1/__init__.py
+++ src/mcp_svr1/__init__.py
@@ -3,16 +3,12 @@
 #
 from importlib.metadata import version as get_version_from_metadata
 
-# __package__ が定義されている場合（通常はパッケージ実行時）、
-# importlib.metadata でバージョンを取得します。
 if __package__:
     __version__ = get_version_from_metadata(__package__)
-# 定義されていない場合（スクリプト直接実行時など）、
-# 仮バージョンを設定します。
 else:
     __version__ = "_._._"
 
 
 __all__ = [
-     "__version__"
+     "__version__",
  ]
